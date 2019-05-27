77ad3a7244beb835b96dcf663c5c0c5728fbe15b
EvgeniyShvets22 <shvets@artstep.com.ua>
2019-05-27 09:19:57 +0300

    adding files

---
+++ app/js/common.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	$('.tabs-nav a').click(function() {
+
+		//Check if the tabs menu has active class
+		$('.tabs-nav li').removeClass('active');
+		$(this).parent().addClass('active');
+	
+		// Display active tab
+		var currentTab = $(this).attr('href');
+		$('.tabs-content > div').hide();
+		$(currentTab).show();
+	
+		return false;
+	  });
+
+
+	  var addTab = $('#addtab');
+	  
+
+	  
+
+});
